b50110d79f90db95b2bc20329d91520d6517741b
Dheeraj Bhumanapalli <dheerajreddylearning@gmail.com>
2026-09-18 19:54:35 +0530

    feat: add initial cpp solution to 1520

---
+++ leetcode/1520/main.cpp
@@ -0,0 +1,86 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<pair<pair<int,int>,string>> validIntervals;
+    unordered_map<char,int> mp;
+    vector<string> ans;
+    int stringsLen = INT_MAX;
+
+    bool isValid(string temp){
+        unordered_map<char,int> freqMap;
+        for(char ch: temp){
+            freqMap[ch]++;
+        }
+
+        for(auto it: freqMap){
+            if(it.second!=mp[it.first]) return false;
+        }
+
+        return true;
+    }
+
+    void getValidSubstrings(int ind, string s){
+        if(ind==s.size()) return;
+
+        string temp = "";
+        for(int i=ind; i<s.size(); i++){
+            temp+=s[i];
+            if(isValid(temp)){
+                validIntervals.push_back({{ind,i+1},temp});
+            }
+        }
+
+        getValidSubstrings(ind+1,s);
+    }
+
+    void maxNonOverlappingSubstrings(int ind, int tempLen, int prevInd, vector<string> temp){
+        if(ind>=validIntervals.size()){
+            if(ans.size()<temp.size()){
+                ans = temp;
+                stringsLen = tempLen;
+            }
+            else if(ans.size()==temp.size()){
+                if(tempLen<stringsLen){
+                    ans = temp;
+                    stringsLen = tempLen;
+                }
+            }
+            return;
+        }
+
+        if(temp.size()==0 || prevInd<=validIntervals[ind].first.first){
+            temp.push_back(validIntervals[ind].second);
+            maxNonOverlappingSubstrings(validIntervals[ind].first.second,tempLen+validIntervals[ind].second.size(),validIntervals[ind].first.second,temp);
+            temp.pop_back();
+        }
+        maxNonOverlappingSubstrings(ind+1,tempLen,prevInd,temp);
+    }
+
+    vector<string> maxNumOfSubstrings(string s) {
+        for(int i=0; i<s.size(); i++){
+            mp[s[i]]++;
+        }
+
+        getValidSubstrings(0,s);
+
+        sort(validIntervals.begin(),validIntervals.end());
+
+        vector<string> temp;
+        maxNonOverlappingSubstrings(0,0,-1,temp);
+
+        return ans;
+    }
+};
+
+int main(){
+    string s = "adefaddaccc";
+    Solution soln;
+    vector<string> ans = soln.maxNumOfSubstrings(s);
+    return 0;
+}
